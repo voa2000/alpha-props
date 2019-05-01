41f6563d41c8d29165dc7706ea76a6df1d09398e
buketkonuk <bukikir@gmail.com>
2019-05-01 17:38:48 +0100

    added test P element to test bootstrap

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="display-1">Testing</p>
         <a
           className="App-link"
           href="https://reactjs.org"
