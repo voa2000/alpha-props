f211a738625b9f77637d8d49c0a489ba36245f54
voa2000 <voa2000@hotmail.com>
2019-05-02 16:47:55 +0100

    create body component

--- src/App.js
+++ src/App.js
@@ -5,25 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Body />
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="display-1">Testing</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> 65cb687e9e053791a412bd1252bd02d1239215ec
     </div>
   );
 }
--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import './Body.css';
+import prop1 from '../images/property1.jpg';
+import prop2 from '../images/property2.jpg';
+import prop3 from '../images/property3.jpg';
 
 function Body() {
   return (
-    <div className="App">
+    <div>
+        <h1>Feature Properties</h1>
 
+        <div class="container">
+        <div class="row">
+        <div class="col">
+    
+        <div class="card">
+         <img class="rounded mx-auto d-block" src={prop1} alt="Property 1" />
+        <div class="card-body">
+         <p class="card-text">
+         The apartments at Lyons Place are spacious, light and quiet, designed with an elegant, 
+         understated flair and fine attention to detail.
+         </p>
+         </div>
+        </div>
     </div>
+    <div class="col">
+    <div class="card">
+    <img class="rounded mx-auto d-block" src={prop2} alt="Property 2" />        
+    <div class="card-body">
+         <p class="card-text">
+         The attention to detail and hand selected materials are inclusive of timber, natural stone and impeccable craft elements.
+         </p>
+         </div>
+        </div>
+    </div>
+    <div class="col">
+    <div class="card">
+    <img class="rounded mx-auto d-block" src={prop3} alt="Property 3" />
+
+    <div class="card-body">
+         <p class="card-text">This property was designed by renowned architect Sir Terry Farrell, considered to be one of London’s best Central London's scheme.</p>
+         </div>
+        </div>
+            </div>
+  </div>
+</div>
+        </div>
+    
   );
 }
 
