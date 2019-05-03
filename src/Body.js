094b53f2c70b6f1e2ed6597b90414c5f776c11b7
Vilius Vystartas <vilius.vystartas@gmail.com>
2019-05-03 14:17:42 +0100

    Updated class to className in components

--- src/Body.js
+++ src/Body.js
@@ -9,36 +9,36 @@ function Body() {
     <div>
         <h1>Featured Properties</h1>
 
-        <div class="container">
-        <div class="row">
-        <div class="col">
+        <div className="container">
+        <div className="row">
+        <div className="col">
     
-        <div class="card">
-         <img class="rounded mx-auto d-block" src={prop1} alt="Property 1" />
-        <div class="card-body">
-         <p class="card-text">
+        <div className="card">
+         <img className="rounded mx-auto d-block" src={prop1} alt="Property 1" />
+        <div className="card-body">
+         <p className="card-text">
          The apartments at Lyons Place are spacious, light and quiet, designed with an elegant, 
          understated flair and fine attention to detail.
          </p>
          </div>
         </div>
     </div>
-    <div class="col">
-    <div class="card">
-    <img class="rounded mx-auto d-block" src={prop2} alt="Property 2" />        
-    <div class="card-body">
-         <p class="card-text">
+    <div className="col">
+    <div className="card">
+    <img className="rounded mx-auto d-block" src={prop2} alt="Property 2" />        
+    <div className="card-body">
+         <p className="card-text">
          The attention to detail and hand selected materials are inclusive of timber, natural stone and impeccable craft elements.
          </p>
          </div>
         </div>
     </div>
-    <div class="col">
-    <div class="card">
-    <img class="rounded mx-auto d-block" src={prop3} alt="Property 3" />
+    <div className="col">
+    <div className="card">
+    <img className="rounded mx-auto d-block" src={prop3} alt="Property 3" />
 
-    <div class="card-body">
-         <p class="card-text">This property was designed by renowned architect Sir Terry Farrell, considered to be one of London’s best Central London's scheme.</p>
+    <div className="card-body">
+         <p className="card-text">This property was designed by renowned architect Sir Terry Farrell, considered to be one of London’s best Central London's scheme.</p>
          </div>
         </div>
             </div>
