910be161fa0330bcbe338c103afe2a530be4cbd9
voa2000 <voa2000@hotmail.com>
2019-05-14 09:55:03 +0100

    Made final changes to box.

--- src/components/Home/Body/Body.js
+++ src/components/Home/Body/Body.js
@@ -10,26 +10,34 @@ function Body() {
       <h1>Featured Properties</h1>
       <div className="body-container">
         <div className="body-box">
-          <img src={prop1} alt="Property 1" />
-          <p>
-            The apartments at Lyons Place are spacious, light and quiet,
-            designed with an elegant, understated flair and fine attention to
-            detail.
-          </p>
+          <div className="body-card">
+            <img src={prop1} alt="Property 1" />
+            <p>
+              The apartments at Lyons Place are spacious, light and quiet,
+              designed with an elegant, understated flair and fine attention to
+              detail.
+            </p>
+          </div>
         </div>
         <div className="body-box">
-          <img src={prop2} alt="Property 2" />
-          <p>
-            The attention to detail and hand selected materials are inclusive of
-            timber, natural stone and impeccable craft elements.
-          </p>
+          <div className="body-card">
+            <img src={prop2} alt="Property 2" />
+            <p>
+              The attention to detail and hand selected materials are inclusive
+              of timber, natural stone and impeccable craft elements.
+            </p>
+          </div>
         </div>
+
         <div className="body-box">
-          <img src={prop3} alt="Property 3" />
-          <p>
-            This property was designed by renowned architect Sir Terry Farrell,
-            considered to be one of London’s best Central London's scheme.
-          </p>
+          <div className="body-card">
+            <img src={prop3} alt="Property 3" />
+            <p>
+              This property was designed by renowned architect Sir Terry
+              Farrell, considered to be one of London’s best Central London's
+              scheme.
+            </p>
+          </div>
         </div>
       </div>
     </div>
