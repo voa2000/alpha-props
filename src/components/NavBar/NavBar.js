fbfcd8bbe7046cb2e3ede86b71d17f6c1c5b19fd
Gawah Chan <gawahchan@gmail.com>
2019-05-16 17:12:15 +0100

    Reworked NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,15 +5,20 @@ import './NavBar.css';
 
 const NavBar = () => (
   <div className="navigation">
-    <div className="container">
-      <nav to="/" className="navigation_items">
-        <ul>
-            <Link to={'/'} >Home </Link>
-            <Link to={'/About'} >About </Link>
-            <Link to={'/Search'} >Search </Link>
-            <Link to={'/Login'} >Login </Link>
-        </ul>
-      </nav>
+    <div className="navigation-title">
+      <h1>Alpha-props</h1>
+    </div>
+    <nav to="/" className="navigation_items">
+        <Link to={'/'} >Home </Link>
+        <Link to={'/About'} >About </Link>
+        <Link to={'/Search'} >Search </Link>
+        <Link to={'/Login'} >Login </Link>
+    </nav>
+    <div className="navigation-inputbox">
+      <input
+        type="text"
+        placeholder="Search"
+      />
     </div>
   </div>
 );
