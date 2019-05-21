cb8094aca3df3a3e1bd2b124bf7da03ae80d283b
Gawah Chan <gawahchan@gmail.com>
2019-05-21 22:29:16 +0100

    Reworked body and footer CSS

--- src/frontend/components/Body.js
+++ src/frontend/components/Body.js
@@ -7,41 +7,37 @@ import prop3 from "../common/images/property3.jpg";
 
 function Body() {
   return (
-    <div>
+    <div className="body-container">
       <h1>Featured Properties</h1>
-      <div className="container">
-        <div className="body-container">
-          <div className="body-card">
-            <Link to="/lyon">
-              <img src={prop1} alt="Property 1" />
-            </Link>
-            <p>
-              The apartments at Lyons Place are spacious, light and quiet,
-              designed with an elegant, understated flair and fine attention to
-              detail.
-            </p>
-          </div>
-          <div className="body-card">
-            <Link to="/terry">
-              <img src={prop2} alt="Property 1" />
-            </Link>
-
-            <p>
-              The attention to detail and hand selected materials are inclusive
-              imber, natural stone and impeccable craft elements.
-            </p>
-          </div>
-          <div className="body-card">
-            <Link to="/timber">
-              <img src={prop3} alt="Property 1" />
-            </Link>
-
-            <p>
-              This property was designed by renowned architect Sir Terry
-              Farrell, considered to be one of London’s best Central London's
-              scheme.
-            </p>
-          </div>
+      <div className="body-properties">
+        <div className="body-card">
+          <Link to="/lyon">
+            <img src={prop1} alt="Property 1" />
+          </Link>
+          <p>
+            The apartments at Lyons Place are spacious, light and quiet,
+            designed with an elegant, understated flair and fine attention to
+            detail.
+          </p>
+        </div>
+        <div className="body-card">
+          <Link to="/terry">
+            <img src={prop2} alt="Property 1" />
+          </Link>
+          <p>
+            The attention to detail and hand selected materials are inclusive
+            imber, natural stone and impeccable craft elements.
+          </p>
+        </div>
+        <div className="body-card">
+          <Link to="/timber">
+            <img src={prop3} alt="Property 1" />
+          </Link>
+          <p>
+            This property was designed by renowned architect Sir Terry
+            Farrell, considered to be one of London’s best Central London's
+            scheme.
+          </p>
         </div>
       </div>
     </div>
