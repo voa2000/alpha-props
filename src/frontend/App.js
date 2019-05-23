5fe2f2bacc7bbb742c58bb1914410212af397fec
buketkonuk <bukikir@gmail.com>
2019-05-23 17:40:54 +0100

    dynamic properties pages from fetch api

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import PropOne from './components/PropOne';
-import PropTwo from './components/PropTwo';
-import PropThree from './components/PropThree';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import './App.css';
@@ -11,6 +8,7 @@ import About from './components/About';
 import Login from './components/Login';
 import Footer from './components/Footer';
 import NavbarSpacer from "./components/NavbarSpacer";
+import PropertyPage from "./components/PropertyPage";
 
 function App() {
   return (
@@ -24,9 +22,7 @@ function App() {
             <Route exact path="/Search" component={Search} />
             <Route exact path="/About" component={About} />
             <Route exact path="/Login" component={Login} />
-            <Route exact path="/lyon" component={PropOne} />
-            <Route exact path="/terry" component={PropTwo} />
-            <Route exact path="/timber" component={PropThree} />
+            <Route exact path="/:propertyId" component={PropertyPage} />
           </Switch>
           <Footer />
         </div>
--- src/frontend/components/Body.js
+++ src/frontend/components/Body.js
@@ -1,47 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "./Body.css";
-import prop1 from "../common/images/property1.jpg";
-import prop2 from "../common/images/property2.jpg";
-import prop3 from "../common/images/property3.jpg";
+import Property from "./Property";
 
-function Body() {
-  return (
-    <div className="body-container">
-      <h1>Featured Properties</h1>
-      <div className="body-properties">
-        <div className="body-card">
-          <Link to="/lyon">
-            <img src={prop1} alt="Property 1" />
-          </Link>
-          <p>
-            The apartments at Lyons Place are spacious, light and quiet,
-            designed with an elegant, understated flair and fine attention to
-            detail.
-          </p>
+export default class Body extends React.Component {
+  constructor() {
+      super();
+      this.state = { propertyList: []};
+  }
+  
+  componentDidMount() {
+      fetch("/api/properties/")
+        .then(res => res.json())
+        // .then(propertyList => console.log(propertyList))
+        .then(propertyList => this.setState({propertyList}))
+        // .then(propertyList => this.setState({propertyList :[{price: 'free'}]}))
+        .catch(e => console.log(e))
+     
+  }
+  render() {
+    return (
+        <div className="body-container">
+          <h1>Featured Properties</h1>
+          <div className="body-properties">
+
+                {this.state.propertyList.map( property => (
+                  <Property
+                    key={property.id}
+                    id= {property.id}
+                    type={property.type}
+                    price={property.price}
+                    />
+                ))
+               
+                }         
+          </div>
         </div>
-        <div className="body-card">
-          <Link to="/terry">
-            <img src={prop2} alt="Property 1" />
-          </Link>
-          <p>
-            The attention to detail and hand selected materials are inclusive
-            imber, natural stone and impeccable craft elements.
-          </p>
-        </div>
-        <div className="body-card">
-          <Link to="/timber">
-            <img src={prop3} alt="Property 1" />
-          </Link>
-          <p>
-            This property was designed by renowned architect Sir Terry
-            Farrell, considered to be one of London’s best Central London's
-            scheme.
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+      );
+  }
+    
 }
 
-export default Body;
