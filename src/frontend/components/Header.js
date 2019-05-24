ab413d3be5c91b5f45a56db24bf68f6b15cfb3e3
Gawah Chan <gawahchan@gmail.com>
2019-05-24 11:54:36 +0100

    ReWorked NavBar to be responsive

--- src/frontend/components/Header.js
+++ src/frontend/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
     <div className="header-container">
       <div className="overlay">
         <h1>ALPHA PROPERTIES</h1>
-        <h3>Your first stop for a Prime Propery in Central London</h3>
+        <h3>Your first stop for a Prime Property in Central London</h3>
       </div>
     </div>
   );
--- src/frontend/components/Property.js
+++ src/frontend/components/Property.js
@@ -10,8 +10,8 @@ export default function Property(props) {
                         <img className="rounded mx-auto d-block" src={require(`../common/images/${props.id}.jpg`)}  alt={`${props.title}`} />
                         <div className="card-body">
                             <p className="card-text">
-                                {/* <p>Property ID: {props.id}</p> */}
-                                <p>Property TYPE: {props.type}</p>
+                                <p>Property ID: {props.id}</p>
+                                <p>{props.bedrooms} bedrooms {props.type}</p>
                                 <p>Property PRICE: {props.price}</p>
                             </p>
                         </div>
