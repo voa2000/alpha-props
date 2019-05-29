39784c60ccb3d999c9f726c82e1c30736bb68591
Gawah Chan <47946026+GawahChan@users.noreply.github.com>
2019-05-29 17:15:12 +0100

    made homepage work for resolutions higher than me (#82)
    Great work Gawah.

--- src/frontend/components/PropertyPageStack.js
+++ src/frontend/components/PropertyPageStack.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './PropertyPageStack.css';
 import Map from "./Map";
-import LoadingIndicator from "./LoadingIndicator";
 
 function PropertyPageStack(props) {
   return (
